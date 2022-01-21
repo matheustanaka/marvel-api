9c13c550407e60af037c64c1524cb9f2b1381f79
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-21 16:16:24 -0300

    feat: add axios get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import axios from "axios";
+import md5 from "md5";
+
 import "./styles/app.scss";
 
+const baseUrl = 'http://gateway.marvel.com/v1/public/characters?';
+const time = Number(new Date());
+const hash = md5(time + `${process.env.REACT_APP_PRIVATEKEY}` + `${process.env.REACT_APP_PUBLICKEY}`);
+
 export function App() {
+
+  useEffect(() => {
+    axios.get(`${baseUrl}ts=${time}&apikey=${process.env.REACT_APP_PUBLICKEY}&hash=${hash}`)
+    .then((response)=> console.log(response.data.data))
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
-    <div className="App">
-      <h1>Hello World 😎</h1>
-    </div>
+    <>
+      <h1>Marvel Api</h1>
+    </>
   );
 }
