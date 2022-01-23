c334be2a49d80ae2a6f8a8b58f21fc2930cd4512
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-22 21:14:32 -0300

    refactor: change html structure

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -11,34 +11,43 @@ const time = Number(new Date());
 const hash = md5(time + `${process.env.REACT_APP_PRIVATEKEY}` + `${process.env.REACT_APP_PUBLICKEY}`);
 
 interface Character {
-    id: number;
-    name: string;
-    description: string;
-    thumbnail: {
-        path: string,
-        extension: string
-    };
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: {
+    path: string,
+    extension: string
+  };
 }
 
 export function CharactersList() {
 
-  const [characters, setCharacters] = useState<Character[]>([]);  
-    
+  const [characters, setCharacters] = useState<Character[]>([]);
+
   useEffect(() => {
     axios.get(`${baseUrl}ts=${time}&apikey=${process.env.REACT_APP_PUBLICKEY}&hash=${hash}`)
-    .then((response)=> setCharacters(response.data.data.results))
-    .catch((err) => console.log(err));
+      .then((response) => setCharacters(response.data.data.results))
+      .catch((err) => console.log(err));
   }, [])
 
   return (
-    <section className="characters-list">
-        <h1>Characters List</h1>
+    <div className="characters-list">
 
+      <header>
+        <h1>Characters List</h1>
+      </header>
+
+      <div className="result-character">
         <ul>
-            {characters.map(character => {
-                return <CharactersItem key={character.name} character={character} />
-            })}
+          {characters.map(character => {
+            return <div className="card-character">
+              <CharactersItem key={character.name} character={character} />
+            </div>
+
+          })}
         </ul>
-    </section>
+      </div>
+    </div>
+
   )
 }
